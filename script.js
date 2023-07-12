f61aa322e68577ceb4e0c1a5dfb426365bcc0ebb
RolphLoph-the-3rd <thedavincicode1347@gmail.com>
2023-07-12 03:13:23 -0400

    first pig game push
    The dice game does not work anymore with the live server in vs code. Need to see if this is a code problem or computer.

---
+++ script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// !SELECTING ELEMENTS
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
+const score0EL = document.querySelector('.score--0');
+const score1EL = document.querySelector('.score--1');
+const current0EL = document.querySelector('.current--0');
+const current1EL = document.querySelector('.current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+// ~STARTING CONDITIONS
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0EL.textContent = 0;
+  score1El.textContent = 0;
+  current0EL.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  player0EL.classList.remove('player--active');
+  player1EL.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// ! ROLLING DICE FUNCTIONALITY
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // Generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // check for roll of 1
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //  add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] +currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // check is players score is >= winningScore
+    if (scores[activePlayer] >= 100) {
+      // finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove(`player--active`);
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
